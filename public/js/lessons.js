516b93e6568e014a0b43cb597c885bd477e7e4de
ffmaer <ffmaer@gmail.com>
2018-12-28 20:07:26 +0800

    simplify

--- public/js/lessons.js
+++ public/js/lessons.js
@@ -9,27 +9,6 @@ function rndColor(){
 	return color[Math.floor(Math.random()*color.length)];
 }
 
-
-// let p = document.querySelectorAll("p");
-// for(let i=0;i<p.length;i++){
-// 	if(p[i].querySelectorAll("a").length==0){
-// 		let text = p[i].textContent;
-// 		p[i].textContent = "";
-// 		for(let j=0;j<text.length;j++){
-// 			let span = document.createElement("span");
-// 			let char = text.charAt(j);
-// 			span.textContent = char;
-// 			if(char == "o"){
-// 				span.style.opacity = 1;
-// 				span.onmouseover = function(){
-// 					span.innerHTML = "◍";
-// 				}
-// 			}
-// 			p[i].append(span);
-// 		}
-// 	}
-// }
-
 let fade_interval;
 let h2 = document.querySelectorAll("h2");
 for(let i=0;i<h2.length;i++){
@@ -114,7 +93,6 @@ for(let i=0;i<links.length;i++){
 	}
 }
 
-
 let h3s = document.querySelectorAll("h3");
 for(let i=0;i<h3s.length;i++){
 	let h3=h3s[i];
@@ -124,52 +102,10 @@ for(let i=0;i<h3s.length;i++){
 	}
 }
 
-let sentence = "My computer program is like a piano. I could continue to use it creatively all my life. - John Whitney";
-sentence = sentence.replace(/ /g,"");
-let sentence_index = 0;
-function nextChar(){
-	let nc = sentence.charAt(sentence_index);
-	sentence_index++;
-	sentence_index = sentence_index % sentence.length;
-	return nc;
-}
-
-let grid_size =20;
-let display_width = page_width;
-let display_height = grid_size*10;
-let display = document.getElementById("display");
-for(let x=0;x<display_width/grid_size;x++){
-	for(let y=0;y<display_height/grid_size;y++){
-		let grid = document.createElement("div");
-		grid.style["background-color"]=rndColor();
-		grid.style.opacity = 0.05;
-		grid.style.width = grid_size;
-		grid.style.height = grid_size;
-		grid.classList.add("grid");
-
-		
-		
-		grid.style["color"] ="white";
-		grid.textContent = ".";
-		grid.setAttribute("char",".");
-		grid.onmouseover = function(){
-			let char = grid.getAttribute("char");
-			if(char == "."){
-				let nc = nextChar();
-				grid.textContent = nc;
-				grid.setAttribute("char",nc);
-				grid.style.opacity = 1;
-			}
-		}
-		display.append(grid);
-	}
-}
-
 let body = document.querySelector("body");
 let gif_index = Math.floor(Math.random()*2);
 body.style.background = `url('/gif/g${gif_index}.gif')`;
 
-let h1_flash_interval;
 let h1s = document.querySelectorAll("h1");
 for(let i=0;i<h1s.length;i++){
 	let h1 = h1s[i];
@@ -186,17 +122,6 @@ for(let i=0;i<h1s.length;i++){
 		div.style.background = rndColor();
 		div.style["font-size"] = "2em";
 		div.classList.add("char");
-		div.onmouseover = function(){
-			clearInterval(h1_flash_interval);
-			h1_flash_interval = setInterval(function(){
-				div.style.background = rndColor();
-				let size = div.style["font-size"].replace(/em/,"");
-				div.style["font-size"]=`${Number.parseFloat(size)+0.01}em`;
-			},20);
-		}
-		div.onmouseout = function(){
-			clearInterval(h1_flash_interval)
-		}
 		h1.append(div);
 	}
 }
@@ -225,9 +150,3 @@ for(let i=0;i<hrs.length;i++){
 	hr.style["font-size"] = "0.1em";
 	hr.textContent = dots(60);
 }
-
-
-
-
-
-
